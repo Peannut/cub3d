4d84d4ef56f84d82be9dbfb2a87b3bdeeec009b5
Peannut <zakariaspam0@gmail.com>
2022-12-31 02:28:23 +0100

    added l gird line sf ama go to sleep

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zoukaddo <zoukaddo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 16:59:56 by zoukaddo          #+#    #+#             */
-/*   Updated: 2022/12/31 02:15:20 by zoukaddo         ###   ########.fr       */
+/*   Updated: 2022/12/31 02:26:46 by zoukaddo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,25 @@
 #include "cub3d.h"
 
 
+// void render(void *mlx, void *window, char map[10][10], int screen_width, int screen_height) {
+//     int x, y, i, j;
+//     int color;
+
+//     for (y = 0; y < 10; y++) {
+//         for (x = 0; x < 10; x++) {
+//             if (map[y][x] == '1') {
+//                 color = 0xA020F0; 
+//             } else {
+//                 color = 0xFFFF00; 
+//             }
+//             for (i = 0; i < 32; i++) {
+//                 for (j = 0; j < 32; j++) {
+//                     mlx_pixel_put(mlx, window, x * 32 + i, y * 32 + j, color);
+//                 }
+//             }
+//         }
+//     }
+// }
 void render(void *mlx, void *window, char map[10][10], int screen_width, int screen_height) {
     int x, y, i, j;
     int color;
@@ -29,6 +48,13 @@ void render(void *mlx, void *window, char map[10][10], int screen_width, int scr
                 for (j = 0; j < 32; j++) {
                     mlx_pixel_put(mlx, window, x * 32 + i, y * 32 + j, color);
                 }
+            }
+            // draw grid
+            for (i = 0; i < 32; i++) {
+                mlx_pixel_put(mlx, window, x * 32 + i, y * 32 + 31, 0x000000);
+            }
+            for (j = 0; j < 32; j++) {
+                mlx_pixel_put(mlx, window, x * 32 + 31, y * 32 + j, 0x000000);
             }
         }
     }
