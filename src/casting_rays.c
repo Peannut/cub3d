74b7da4b9ca047274beadfd32c6340415583737d
Peannut <zakariaspam0@gmail.com>
2023-01-15 17:05:52 +0100

    rays t castaw using Bresenham line algo

--- src/casting_rays.c
+++ src/casting_rays.c
@@ -13,20 +13,52 @@
 #include "../cub3d.h"
 
 
-void castRay(void *mlx, void *window, t_data *data, int col) {
-    int x1 = data->player.x * 32 + 16;  // x coordinate of the player's position
-    int y1 = data->player.y * 32 + 16;  // y coordinate of the player's position
+// void castRay(void *mlx, void *window, t_data *data, int col) {
+//     int x1 = data->player.x * 32 + 16;  // x coordinate of the player's position
+//     int y1 = data->player.y * 32 + 16;  // y coordinate of the player's position
 
-    // calculate the angle of the ray relative to the player's direction
+//     // calculate the angle of the ray relative to the player's direction
+//     double angle = data->player.rotationAngle - FOV_ANGEL / 2 + (double)col / (data->width * BLOCK )* FOV_ANGEL;
+
+//     // find the intersection point of the ray with the closest wall
+//     int x2 = x1 + 70 * cos(angle);
+//     int y2 = y1 + 70 * sin(angle);
+
+//     // draw the ray as a line
+//     mlx_pixel_put(mlx, window, x1, y1, 0xFF0000);
+//     mlx_pixel_put(mlx, window, x2, y2, 0xFF0000);
+// }
+void castRay(void *mlx, void *window, t_data *data, int col) {
+    // player position
+    int x1 = data->player.x * BLOCK + 16;
+    int y1 = data->player.y * BLOCK + 16;
+
+    // angle of the ray
     double angle = data->player.rotationAngle - FOV_ANGEL / 2 + (double)col / (data->width * BLOCK )* FOV_ANGEL;
 
-    // find the intersection point of the ray with the closest wall
     int x2 = x1 + 70 * cos(angle);
     int y2 = y1 + 70 * sin(angle);
 
-    // draw the ray as a line
-    mlx_pixel_put(mlx, window, x1, y1, 0xFF0000);
-    mlx_pixel_put(mlx, window, x2, y2, 0xFF0000);
+    int x = x1, y = y1;
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = x1 < x2 ? 1 : -1;
+    int sy = y1 < y2 ? 1 : -1;
+    int err = dx - dy;
+    int e2;
+
+    while(x != x2 || y != y2) {
+        mlx_pixel_put(mlx, window, x, y, 0xFF0000);
+        e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y += sy;
+        }
+    }
 }
 
 void castAllRays(t_data *data) {
